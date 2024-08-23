83f2b8f83cdf6aef9e4116ec8c670c593d060e77
Bulazs <mcbulazslol@gmail.com>
2024-08-23 20:12:00 +0200

    setting header text on chacracter sheet

--- frontend/src/pages/characters/CharacterSheet/CharacterSheet.tsx
+++ frontend/src/pages/characters/CharacterSheet/CharacterSheet.tsx
@@ -1,9 +1,13 @@
 import type React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useGetCharacterByIdQuery } from "../../../store/api/characterApiSlice";
+import { setHeaderText } from "../../../store/utility/headerSlice";
 
 const CharacterSheet: React.FC = () => {
+	const dispatch = useDispatch();
 	const { characterId } = useParams();
 	if (!characterId || Number.isNaN(Number.parseInt(characterId))) {
 		return <div>Invalid character ID</div>;
@@ -13,18 +17,22 @@ const CharacterSheet: React.FC = () => {
 		error,
 		isLoading,
 	} = useGetCharacterByIdQuery(Number.parseInt(characterId));
+    useEffect(() => {
+        dispatch(setHeaderText(character?.name ?? "Character Sheet"));
+    }, [dispatch, character]);
+    
 	if (isLoading) {
-		return <div>Loading...</div>;
+        return <div>Loading...</div>;
 	}
 	if (error) {
-		toast("Error loading character", { type: "error" });
-        console.error("Error loading character", error);
+        toast("Error loading character", { type: "error" });
+		console.error("Error loading character", error);
 		return <div>Error loading character</div>;
 	}
 	if (!character) {
 		return <div>Character not found</div>;
 	}
-    console.log(character);
+    console.log(character)
 	return (
 		<div>
 			<h1>Character Sheet</h1>
